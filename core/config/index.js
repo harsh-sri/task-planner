7f02ca55d6b74213ffd18da83fd2c235dfb718ae
Harsh <harsh.srivastava51@gmail.com>
2024-07-01 17:24:18 +0200

    --refactored config module and added db helper functions

--- core/config/index.js
+++ core/config/index.js
@@ -1,2 +1,27 @@
-const dotenv = require("dotenv");
+import * as dotenv from "dotenv";
 dotenv.config();
+
+/**
+ *
+ * @returns {db} object with mongo uri
+ */
+export const mongo = () => {
+  return {
+    uri: process.env.MONGO_URI,
+  };
+};
+
+/**
+ *
+ * @returns {messages} object to simplify cli-messages throughout the application
+ */
+export const messages = () => {
+  return {
+    db: {
+      waiting_to_connect:
+        "establishing connection to the database, please wait...",
+      connected: "connected to the database",
+      disconnect: "database disconnected.",
+    },
+  };
+};
---
+++ core/db/index.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+import chalk from "chalk";
+import ora from "ora";
+import * as config from "../config/index.js";
+
+/**
+ * Function to connect to the database
+ */
+export const connect = async () => {
+  const mongo = config.mongo();
+  const {
+    db: { waiting_to_connect, connected },
+  } = config.messages();
+  try {
+    const spinner = ora(waiting_to_connect).start();
+    await mongoose.connect(mongo.uri);
+    spinner.stop();
+    console.log(chalk.greenBright(connected));
+  } catch (err) {
+    console.log(chalk.redBright("err: "), err);
+    process.exit(1);
+  }
+};
+
+/**
+ * Function to disconnect. Can be useful in clean-shutdown process
+ */
+export const disconnect = async () => {
+  const {
+    db: { disconnect },
+  } = config.messages();
+  try {
+    await mongoose.disconnect();
+    console.log(chalk.greenBright(disconnect));
+  } catch (err) {
+    console.log(chalk.redBright("err: "), err);
+    process.exit(1);
+  }
+};
